eee2b7334af597d71bd3a2a06cd5a6e0a49627a4
malahimaxtra <malahimaxtra@gmail.com>
2025-06-14 15:51:34 +0500

    ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <h1 className="text-4xl font-extrabold  sm:text-5xl text-muted-foreground bg-clip-text">
           Progressive UI, Hatched for the Future.
         </h1>
-        <p className="text-muted-foreground mt-2 text-lg leading-relaxed">
+        <p className="text-muted-foreground mx-2 mt-2 text-lg leading-relaxed">
           A ShadCN UI registry built for <strong>Next.js 15+</strong> and <strong>React 19</strong>.  
         </p>
         <p className="text-muted-foreground">
